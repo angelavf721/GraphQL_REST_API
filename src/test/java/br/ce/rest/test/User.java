4c80bb6e1b35b4d87931fa5b068fb0cd9d60592f
Angel <angelavasconcelos780@gmail.com>
2023-03-29 15:15:04 -0300

    Adição de testes de erros

--- src/test/java/br/ce/rest/test/User.java
+++ src/test/java/br/ce/rest/test/User.java
@@ -9,8 +9,13 @@ import java.util.HashMap;
 import java.util.Map;
 
 import static io.restassured.RestAssured.given;
+import static org.hamcrest.CoreMatchers.*;
+import static org.hamcrest.Matchers.hasSize;
+
+
 @FixMethodOrder(MethodSorters.NAME_ASCENDING)
 public class User extends Base {
+
     private static Integer userId;
     @Test
     public void t01_listarUsuario(){
@@ -18,14 +23,14 @@ public class User extends Base {
         .when()
             .get("/users")
         .then()
-                .log().all()
+            .log().all()
             .statusCode(200);
     }
     @Test
     public void t02_criarUsuario(){
         Map<String, String> user = new HashMap<>();
         user.put("name","Maria Do Carmo");
-        user.put("email","MariadoCarmo@gamail.com");
+        user.put("email","MariaDOC0arm2o@gmail.com");
         user.put("gender","male");
         user.put("status","active");
         userId = given()
@@ -33,7 +38,6 @@ public class User extends Base {
                 .when()
                 .post("/users")
                 .then()
-                .log().all()
                 .statusCode(201)
                 .extract().path("id");
     }
@@ -50,11 +54,22 @@ public class User extends Base {
                 .when()
                 .put("/users/{id}")
                 .then()
-                .log().all()
                 .statusCode(200);
     }
     @Test
-    public void t04_ExcluirUsuario(){
+    public void t04_naoEditarUsuarioComEmailExistente(){
+        Map<String, String> user = new HashMap<>();
+        user.put("email","patel_bhargava@murray-kihn.com");
+        given()
+                .body(user)
+                .pathParam("id", userId)
+                .when()
+                .put("/users/{id}")
+                .then()
+                .statusCode(422);
+    }
+    @Test
+    public void t05_ExcluirUsuario(){
         given()
                 .pathParam("id", userId)
                 .when()
@@ -62,5 +77,35 @@ public class User extends Base {
                 .then()
                 .statusCode(204);
     }
+    @Test
+    public void t06_naoCriarUsuarioComMesmoEmail(){
+        Map<String, String> user = new HashMap<>();
+        user.put("name","Maria Do Carmo");
+        user.put("email","marar_gita_rep@turcotte.com");
+        user.put("gender","male");
+        user.put("status","active");
+        given()
+                .body(user)
+                .when()
+                .post("/users")
+                .then()
+                .statusCode(422)
+                .body("$",hasSize(1))
+                .body("field", hasItem("email"))
+                .body("message", hasItem("has already been taken"));
+    }
+    @Test
+    public void t07_naoCriarUsuarioVazio(){
+        given()
+                .when()
+                .post("/users")
+                .then()
+                .statusCode(422)
+                .body("$",hasSize(4))
+                .body("message", hasItems(
+                        "can't be blank", "can't be blank",
+                        "can't be blank, can be male of female","can't be blank"));
+    }
+
 
 }
